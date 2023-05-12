a0c2057e60fea0ba6b494494ca12b0e222f59399
Dave799 <davistibz@gmail.com>
2023-05-12 13:50:16 +0300

    🚀 Organized file structure

---
+++ lib/components/inputTextOnly.comp.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:skyconnect_starter/components/heading_text.dart';
+import 'package:skyconnect_starter/controllers/responsive.dart';
+import 'package:skyconnect_starter/theme/design.theme.dart';
+
+class inputTextOnly extends StatefulWidget {
+  double? size;
+  String? value;
+  bool? hide;
+  String? type;
+  final String hintText;
+  inputTextOnly(
+      {super.key,
+      required this.hintText,
+      this.value,
+      this.type,
+      this.hide,
+      this.size});
+
+  @override
+  State<inputTextOnly> createState() => _inputTextOnlyState();
+}
+
+class _inputTextOnlyState extends State<inputTextOnly> {
+  TextEditingController value = TextEditingController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    value.text = "";
+    if (widget.value != null) value.text = widget.value!;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      child: Flex(
+        direction:
+            Responsive.isDesktop(context) ? Axis.horizontal : Axis.vertical,
+        mainAxisAlignment: Responsive.isDesktop(context)
+            ? MainAxisAlignment.spaceBetween
+            : MainAxisAlignment.start,
+        crossAxisAlignment: Responsive.isDesktop(context)
+            ? CrossAxisAlignment.center
+            : CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: Responsive.isDesktop(context) ? widget.size! : widget.size,
+            height: Responsive.isDesktop(context) ? 40 : 40,
+            child: Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(
+                left: Insets().appPadding / 2,
+                right: Insets().appPadding / 2,
+              ),
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.grey),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(Insets().appPadding / 2)),
+              child: TextFormField(
+                  keyboardType: widget.type == "number"
+                      ? TextInputType.number
+                      : widget.type == "phone"
+                          ? TextInputType.phone
+                          : widget.type == "email"
+                              ? TextInputType.emailAddress
+                              : widget.type == "url"
+                                  ? TextInputType.url
+                                  : widget.type == "datetime"
+                                      ? TextInputType.datetime
+                                      : widget.type == "address"
+                                          ? TextInputType.streetAddress
+                                          : widget.type == "multline"
+                                              ? TextInputType.multiline
+                                              : widget.type == "name"
+                                                  ? TextInputType.name
+                                                  : null,
+                  style: GoogleFonts.openSans(
+                    fontSize: Responsive.isDesktop(context) ? 16 : 14,
+                  ),
+                  obscureText: widget.hide != null ? widget.hide! : false,
+                  controller: value,
+                  onChanged: (val) {
+                    widget.value = val;
+                  },
+                  onEditingComplete: () {
+                    setState(() {
+                      value.text = widget.value!;
+                    });
+                    print(value.text);
+                  },
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: widget.hintText,
+                  )),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
