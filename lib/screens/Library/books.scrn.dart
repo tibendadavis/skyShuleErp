4265c2bbc66bd3a2b60b9e5224b6d10b70ab840c
Dave799 <davistibz@gmail.com>
2023-05-12 14:01:22 +0300

    🚀 Organized file structure

---
+++ lib/screens/Library/books.scrn.dart
@@ -0,0 +1,236 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
+import 'package:path/path.dart';
+import 'package:skyconnect_starter/components/app_drawer/skyShuleDrawer.dart';
+import 'package:skyconnect_starter/components/downloadBar.comp.dart';
+import 'package:skyconnect_starter/components/header.dart';
+import 'package:skyconnect_starter/components/heading1.dart';
+import 'package:skyconnect_starter/components/heading2.dart';
+import 'package:skyconnect_starter/components/heading3.dart';
+import 'package:skyconnect_starter/components/heading4.dart';
+import 'package:skyconnect_starter/components/heading5.dart';
+import 'package:skyconnect_starter/components/heading6.dart';
+import 'package:skyconnect_starter/components/heading_text.dart';
+import 'package:skyconnect_starter/components/searchBar.comp.dart';
+import 'package:skyconnect_starter/components/searchInputOptions.comp.dart';
+import 'package:skyconnect_starter/components/tile2.comp.dart';
+import 'package:skyconnect_starter/components/tile3.comp.dart';
+import 'package:skyconnect_starter/controllers/funcs_main.dart';
+import 'package:skyconnect_starter/controllers/responsive.dart';
+import 'package:skyconnect_starter/screens/addBook.scrn.dart';
+import 'package:skyconnect_starter/screens/addSubject.scrn.dart';
+import 'package:skyconnect_starter/screens/addTerm.scrn.dart';
+import 'package:skyconnect_starter/screens/student_admission.scrn.dart';
+import 'package:skyconnect_starter/theme/design.theme.dart';
+
+class books extends StatefulWidget {
+  const books({super.key});
+
+  @override
+  State<books> createState() => _booksState();
+}
+
+class _booksState extends State<books> {
+  bool _menu = false;
+  double _drawersize = 250;
+  var _class;
+  var _category;
+  TextEditingController _From = TextEditingController();
+  TextEditingController _To = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      child: Expanded(
+          child: Column(children: [
+        Container(
+          alignment: Alignment.bottomLeft,
+          padding: EdgeInsets.only(
+              top: Insets().appPadding,
+              left: Responsive.isDesktop(context)
+                  ? Insets().appPadding * 2
+                  : Insets().appPadding,
+              right: Insets().appGap),
+          child: HeadingText(
+            size: Responsive.isDesktop(context) ? 35 : 30,
+            value: "Books",
+            fontWeight: FontWeight.w700,
+            color: Colors.grey.shade800,
+          ),
+        ),
+        Flex(
+          direction:
+              Responsive.isDesktop(context) ? Axis.horizontal : Axis.vertical,
+          children: [
+            SizedBox(
+                width: Responsive.isDesktop(context)
+                    ? 410
+                    : MediaQuery.of(context).size.width,
+                child: tile3(
+                  icon: Icons.book,
+                  link: addBooks(),
+                  linkTitle: "Add Book",
+                )),
+            SizedBox(
+                width: Responsive.isDesktop(context)
+                    ? 410
+                    : MediaQuery.of(context).size.width,
+                child: tile2(tileHeading: "Total Books", tileData: "7")),
+          ],
+        ),
+        searchBar(
+          title: "Search for Books",
+          opt1: searchInputOptions(
+            title: "Category",
+            opt1: "Borrowed Books",
+            opt2: "Available Books",
+            opt3: "Repairable",
+            opt4: "Discarded Books",
+          ),
+          opt2: searchInputOptions(
+            title: "Class",
+            opt1: " ",
+          ),
+          opt3: searchInputOptions(
+            title: "From",
+            opt1: " ",
+          ),
+          opt4: searchInputOptions(
+            title: "To",
+            opt1: " ",
+          ),
+        ),
+        downloadBar(results: "7"),
+        Expanded(
+          child: ListView(children: [
+            Container(
+              margin: EdgeInsets.only(
+                left: Responsive.isDesktop(context) ? Insets().appPadding : 13,
+                right: Responsive.isDesktop(context) ? Insets().appPadding : 13,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(Insets().appGap + 4)),
+              padding: EdgeInsets.only(
+                  left: 15, right: 15, bottom: Insets().appPadding),
+              child: Center(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                      headingTextStyle:
+                          TextStyle(color: Palette().primaryColor),
+                      horizontalMargin: 0,
+                      columnSpacing: Responsive.isDesktop(context) ? 20 : 10,
+                      columns: [
+                        DataColumn(
+                            label: Checkbox(
+                          value: false,
+                          onChanged: (value) {
+                            setState(() {});
+                          },
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? null : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Book Category",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 150 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Title",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 150 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Book Author",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? null : 130,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Book Quantity",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 80 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Available",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 80 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Issued",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 60 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Lost",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 80 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Binding",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? null : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Reserved",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 150 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Action",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                      ],
+                      rows: []),
+                ),
+              ),
+            ),
+          ]),
+        )
+      ])),
+    );
+  }
+}
