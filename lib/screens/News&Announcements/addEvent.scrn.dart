7321d2da778d395a27cadc15fba7e647c2e7f1b4
Dave799 <davistibz@gmail.com>
2023-05-12 17:08:26 +0300

    🚀 Organized file structure

---
+++ lib/screens/News&Announcements/addEvent.scrn.dart
@@ -0,0 +1,234 @@
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
+import 'package:path/path.dart';
+import 'package:skyconnect_starter/components/app_drawer/skyShuleDrawer.dart';
+import 'package:skyconnect_starter/components/bankDetails.dart';
+import 'package:skyconnect_starter/components/contactsDetails.dart';
+import 'package:skyconnect_starter/components/header.dart';
+import 'package:skyconnect_starter/components/heading2.dart';
+import 'package:skyconnect_starter/components/heading3.dart';
+import 'package:skyconnect_starter/components/heading_text.dart';
+import 'package:skyconnect_starter/components/heading5.dart';
+import 'package:skyconnect_starter/components/heading6.dart';
+import 'package:skyconnect_starter/components/heading_text.dart';
+import 'package:skyconnect_starter/components/inputBigText.comp.dart';
+import 'package:skyconnect_starter/components/inputDate.comp.dart';
+import 'package:skyconnect_starter/components/inputFile.comp.dart';
+import 'package:skyconnect_starter/components/inputOptions.comp.dart';
+import 'package:skyconnect_starter/components/inputTextField.comp.dart';
+import 'package:skyconnect_starter/components/officialDetails.dart';
+import 'package:skyconnect_starter/components/otherFacilities.dart';
+import 'package:skyconnect_starter/components/parentsDetails.dart';
+import 'package:skyconnect_starter/components/personalDetails.dart';
+import 'package:skyconnect_starter/components/uploadDocuments.dart';
+import 'package:skyconnect_starter/controllers/funcs_main.dart';
+import 'package:skyconnect_starter/controllers/responsive.dart';
+import 'package:skyconnect_starter/components/academicDetails.dart';
+import 'package:skyconnect_starter/theme/design.theme.dart';
+
+class addEvent extends StatefulWidget {
+  const addEvent({super.key});
+
+  @override
+  State<addEvent> createState() => _addEventState();
+}
+
+class _addEventState extends State<addEvent>
+    with SingleTickerProviderStateMixin {
+  var picked;
+  bool _menu = false;
+  late AnimationController controller;
+  late Animation<double> scaleAnimation;
+  var _specialGrade;
+  TextEditingController _date = TextEditingController();
+  var _madefor;
+  double _drawersize = 250;
+  @override
+  void initState() {
+    super.initState();
+
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 700));
+    scaleAnimation = CurvedAnimation(
+        parent: controller, curve: Curves.fastLinearToSlowEaseIn);
+
+    controller.addListener(() {
+      setState(() {});
+    });
+
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Center(
+      child: Material(
+        color: Colors.transparent,
+        child: ScaleTransition(
+          scale: scaleAnimation,
+          child: Container(
+            margin: EdgeInsets.only(
+                top: Responsive.isDesktop(context) ? 20 : 50,
+                bottom: Responsive.isDesktop(context) ? 20 : 50),
+            decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0))),
+            child: SingleChildScrollView(
+                child: SizedBox(
+              width: Responsive.isDesktop(context)
+                  ? size.width / 2
+                  : size.width - 20,
+              child: Expanded(
+                  child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    padding: EdgeInsets.only(
+                        top: Insets().appPadding,
+                        left: Responsive.isDesktop(context)
+                            ? Insets().appPadding * 2
+                            : Insets().appPadding,
+                        right: Insets().appPadding),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        HeadingText(
+                          size: Responsive.isDesktop(context) ? 35 : 20,
+                          value: "NEWS & ANNOUNCEMENTS",
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        const Heading3(
+                          value: "News Board",
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.only(
+                        top: Insets().appPadding,
+                        bottom: Insets().appPadding,
+                        left: Insets().appPadding,
+                        right: Insets().appPadding),
+                    margin: EdgeInsets.only(
+                        top: 0,
+                        left: Insets().appPadding,
+                        right: Insets().appPadding,
+                        bottom: Insets().appPadding),
+                    decoration: BoxDecoration(
+                        color: Palette().primaryColorLight,
+                        borderRadius:
+                            BorderRadius.circular(Insets().appRadius)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: !Responsive.isDesktop(context)
+                              ? MediaQuery.of(context).size.width
+                              : MediaQuery.of(context).size.width / 1.8,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              inputTextField(
+                                  title: "Title",
+                                  hintText: "Title of the News"),
+                              SizedBox(
+                                height: Responsive.isDesktop(context) ? 10 : 15,
+                              ),
+                              inputDate(heading: "Date", subheading: "Date"),
+                              SizedBox(
+                                height: Responsive.isDesktop(context) ? 10 : 15,
+                              ),
+                              inputOptions(
+                                title: "News For",
+                                opt1: "All",
+                                opt2: "Students",
+                                opt3: "Teachers",
+                                opt4: "Supporting Teachers",
+                              ),
+                              SizedBox(
+                                height: Responsive.isDesktop(context) ? 10 : 15,
+                              ),
+                              inputFile(heading: "Attachment"),
+                              SizedBox(
+                                height: Responsive.isDesktop(context) ? 15 : 15,
+                              ),
+                              inputBigText(
+                                  title: "News Description",
+                                  hintText: "News Description"),
+                              SizedBox(
+                                height: Responsive.isDesktop(context) ? 10 : 15,
+                              ),
+                              Flex(
+                                direction: Responsive.isDesktop(context)
+                                    ? Axis.horizontal
+                                    : Axis.vertical,
+                                mainAxisAlignment: Responsive.isDesktop(context)
+                                    ? MainAxisAlignment.end
+                                    : MainAxisAlignment.start,
+                                crossAxisAlignment:
+                                    Responsive.isDesktop(context)
+                                        ? CrossAxisAlignment.center
+                                        : CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width: 500,
+                                    height:
+                                        Responsive.isDesktop(context) ? 50 : 40,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          padding: EdgeInsets.only(
+                                            left: Insets().appPadding / 2,
+                                            right: Insets().appPadding / 2,
+                                          ),
+                                          backgroundColor:
+                                              Palette().primaryColor,
+                                          elevation: 0,
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(
+                                                      Insets().appPadding /
+                                                          1.5))),
+                                      onPressed: () {},
+                                      child: Center(
+                                          child: HeadingText(
+                                        size: Responsive.isDesktop(context)
+                                            ? 18
+                                            : 14,
+                                        value: "Save",
+                                      )),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: Responsive.isDesktop(context) ? 10 : 15,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )),
+            )),
+          ),
+        ),
+      ),
+    );
+  }
+}
