ff14f2cab788e22c7c5756abc924328a8f891725
Dave799 <davistibz@gmail.com>
2023-05-12 14:01:19 +0300

    🚀 Organized file structure

---
+++ lib/screens/Library/bookLost.scrn.dart
@@ -0,0 +1,160 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:path/path.dart';
+import 'package:skyconnect_starter/components/app_drawer/skyShuleDrawer.dart';
+import 'package:skyconnect_starter/components/downloadBar.comp.dart';
+import 'package:skyconnect_starter/components/header.dart';
+import 'package:skyconnect_starter/components/heading1.dart';
+import 'package:skyconnect_starter/components/heading2.dart';
+import 'package:skyconnect_starter/components/heading3.dart';
+import 'package:skyconnect_starter/components/heading4.dart';
+import 'package:skyconnect_starter/components/heading5.dart';
+import 'package:skyconnect_starter/components/heading6.dart';
+import 'package:skyconnect_starter/components/heading_text.dart';
+import 'package:skyconnect_starter/components/searchBar.comp.dart';
+import 'package:skyconnect_starter/components/searchInputOptions.comp.dart';
+import 'package:skyconnect_starter/components/tile2.comp.dart';
+import 'package:skyconnect_starter/components/tile3.comp.dart';
+import 'package:skyconnect_starter/controllers/responsive.dart';
+import 'package:skyconnect_starter/screens/addBookLost.scrn.dart';
+import 'package:skyconnect_starter/screens/addSubject.scrn.dart';
+import 'package:skyconnect_starter/screens/addTerm.scrn.dart';
+import 'package:skyconnect_starter/screens/student_admission.scrn.dart';
+import 'package:skyconnect_starter/theme/design.theme.dart';
+
+class bookLost extends StatefulWidget {
+  const bookLost({super.key});
+
+  @override
+  State<bookLost> createState() => _bookLostState();
+}
+
+class _bookLostState extends State<bookLost> {
+  bool _menu = false;
+  double _drawersize = 250;
+  var _classlevel;
+  var _bookTitle;
+  var _stream;
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      child: Expanded(
+          child: Column(children: [
+        Container(
+          alignment: Alignment.bottomLeft,
+          padding: EdgeInsets.only(
+              top: Insets().appPadding,
+              left: Responsive.isDesktop(context)
+                  ? Insets().appPadding * 2
+                  : Insets().appPadding,
+              right: Insets().appGap),
+          child: HeadingText(
+            size: Responsive.isDesktop(context) ? 35 : 30,
+            value: "Books Lost",
+            fontWeight: FontWeight.w700,
+            color: Colors.grey.shade800,
+          ),
+        ),
+        Flex(
+          direction:
+              Responsive.isDesktop(context) ? Axis.horizontal : Axis.vertical,
+          children: [
+            SizedBox(
+                width: Responsive.isDesktop(context)
+                    ? 410
+                    : MediaQuery.of(context).size.width,
+                child: tile3(
+                  icon: Icons.book,
+                  link: addBookLost(),
+                  linkTitle: "Add Lost Book",
+                )),
+            SizedBox(
+                width: Responsive.isDesktop(context)
+                    ? 410
+                    : MediaQuery.of(context).size.width,
+                child: tile2(tileHeading: "Total Books Lost", tileData: "7")),
+          ],
+        ),
+        searchBar(
+          title: "Search for Lost Books",
+          opt1: searchInputOptions(title: "Book Title", opt1: ""),
+        ),
+        downloadBar(results: "7"),
+        Expanded(
+          child: ListView(children: [
+            Container(
+              margin: EdgeInsets.only(
+                left: Responsive.isDesktop(context) ? Insets().appPadding : 13,
+                right: Responsive.isDesktop(context) ? Insets().appPadding : 13,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(Insets().appGap + 4)),
+              padding: EdgeInsets.only(
+                  left: 15, right: 15, bottom: Insets().appPadding),
+              child: Center(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                      headingTextStyle:
+                          TextStyle(color: Palette().primaryColor),
+                      horizontalMargin: 0,
+                      columnSpacing: Responsive.isDesktop(context) ? 20 : 10,
+                      columns: [
+                        DataColumn(
+                            label: Checkbox(
+                          value: false,
+                          onChanged: (value) {
+                            setState(() {});
+                          },
+                        )),
+                        DataColumn(
+                            label: Expanded(
+                          child: HeadingText(
+                            size: 14,
+                            value: "No.",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 650 : 150,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Title",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 250 : 150,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Book No.",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 250 : 150,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Action",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                      ],
+                      rows: []),
+                ),
+              ),
+            ),
+          ]),
+        )
+      ])),
+    );
+  }
+}
