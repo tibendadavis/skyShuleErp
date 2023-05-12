0e68e747588800f44aaacad48e4f22b52bac2e6c
Dave799 <davistibz@gmail.com>
2023-05-12 17:08:39 +0300

    🚀 Organized file structure

--- lib/screens/Payroll Management/employeeBonus.scrn.dart
+++ lib/screens/Payroll Management/employeeBonus.scrn.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -13,11 +12,11 @@ import 'package:skyconnect_starter/components/heading5.dart';
 import 'package:skyconnect_starter/components/heading6.dart';
 import 'package:skyconnect_starter/components/heading_text.dart';
 import 'package:skyconnect_starter/controllers/responsive.dart';
-import 'package:skyconnect_starter/screens/addEmployeeBonus.scrn.dart';
-import 'package:skyconnect_starter/screens/addEmployeeSalary.scrn.dart';
-import 'package:skyconnect_starter/screens/addSubject.scrn.dart';
-import 'package:skyconnect_starter/screens/addTerm.scrn.dart';
-import 'package:skyconnect_starter/screens/student_admission.scrn.dart';
+import 'package:skyconnect_starter/screens/Payroll%20Management/addEmployeeBonus.scrn.dart';
+import 'package:skyconnect_starter/screens/Payroll%20Management/addEmployeeSalary.scrn.dart';
+import 'package:skyconnect_starter/screens/Subjects/addSubject.scrn.dart';
+import 'package:skyconnect_starter/screens/Terms/addTerm.scrn.dart';
+import 'package:skyconnect_starter/screens/Users/student_admission.scrn.dart';
 import 'package:skyconnect_starter/theme/design.theme.dart';
 
 class employeeBonus extends StatefulWidget {
