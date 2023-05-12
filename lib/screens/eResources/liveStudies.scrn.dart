2f7d2b412d93560552480cc13fb64804f07de19b
Dave799 <davistibz@gmail.com>
2023-05-12 17:03:08 +0300

    🚀 Organized file structure

--- lib/screens/eResources/liveStudies.scrn.dart
+++ lib/screens/eResources/liveStudies.scrn.dart
@@ -12,10 +12,10 @@ import 'package:skyconnect_starter/components/heading5.dart';
 import 'package:skyconnect_starter/components/heading6.dart';
 import 'package:skyconnect_starter/components/heading_text.dart';
 import 'package:skyconnect_starter/controllers/responsive.dart';
-import 'package:skyconnect_starter/screens/addClass.scrn.dart';
-import 'package:skyconnect_starter/screens/addClassNotes.scrn.dart';
-import 'package:skyconnect_starter/screens/addLiveSettings.scrn.dart';
-import 'package:skyconnect_starter/screens/student_admission.scrn.dart';
+import 'package:skyconnect_starter/screens/Class/addClass.scrn.dart';
+import 'package:skyconnect_starter/screens/eResources/addClassNotes.scrn.dart';
+import 'package:skyconnect_starter/screens/eResources/addLiveSettings.scrn.dart';
+import 'package:skyconnect_starter/screens/Users/student_admission.scrn.dart';
 import 'package:skyconnect_starter/theme/design.theme.dart';
 
 class liveStudies extends StatefulWidget {
