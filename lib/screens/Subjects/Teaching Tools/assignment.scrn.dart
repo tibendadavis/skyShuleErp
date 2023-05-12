7948d615e29828ca23e356a0296b79ac45f757ef
Dave799 <davistibz@gmail.com>
2023-05-12 14:02:44 +0300

    🚀 Organized file structure

---
+++ lib/screens/Subjects/Teaching Tools/assignment.scrn.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:path/path.dart';
+import 'package:skyconnect_starter/components/app_drawer/skyShuleDrawer.dart';
+import 'package:skyconnect_starter/components/downloadBar.comp.dart';
+import 'package:skyconnect_starter/components/header.dart';
+import 'package:skyconnect_starter/components/heading1.dart';
+import 'package:skyconnect_starter/components/heading2.dart';
+import 'package:skyconnect_starter/components/heading3.dart';
+import 'package:skyconnect_starter/components/heading4.dart';
+import 'package:skyconnect_starter/components/heading5.dart';
+import 'package:skyconnect_starter/components/heading6.dart';
+import 'package:skyconnect_starter/components/heading_text.dart';
+import 'package:skyconnect_starter/components/searchBar.comp.dart';
+import 'package:skyconnect_starter/components/searchInputOptions.comp.dart';
+import 'package:skyconnect_starter/components/tile2.comp.dart';
+import 'package:skyconnect_starter/components/tile3.comp.dart';
+import 'package:skyconnect_starter/controllers/responsive.dart';
+import 'package:skyconnect_starter/screens/addAssignment.scrn.dart';
+import 'package:skyconnect_starter/screens/addStream.scrn.dart';
+import 'package:skyconnect_starter/screens/student_admission.scrn.dart';
+import 'package:skyconnect_starter/theme/design.theme.dart';
+
+class assignment extends StatefulWidget {
+  const assignment({super.key});
+
+  @override
+  State<assignment> createState() => _assignmentState();
+}
+
+class _assignmentState extends State<assignment> {
+  bool _menu = false;
+  double _drawersize = 250;
+  var _classlevel;
+  var _academicYear;
+  var _stream;
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      child: Expanded(
+          child: Column(children: [
+        Container(
+          alignment: Alignment.bottomLeft,
+          padding: EdgeInsets.only(
+              top: Insets().appPadding,
+              left: Responsive.isDesktop(context)
+                  ? Insets().appPadding * 2
+                  : Insets().appPadding,
+              right: Insets().appGap),
+          child: HeadingText(
+            size: Responsive.isDesktop(context) ? 35 : 30,
+            value: "Assignments",
+            fontWeight: FontWeight.w700,
+            color: Colors.grey.shade800,
+          ),
+        ),
+        Flex(
+          direction:
+              Responsive.isDesktop(context) ? Axis.horizontal : Axis.vertical,
+          children: [
+            SizedBox(
+                width: Responsive.isDesktop(context)
+                    ? 410
+                    : MediaQuery.of(context).size.width,
+                child: tile3(
+                  icon: Icons.assignment,
+                  link: addAssignment(),
+                  linkTitle: "Add Assignment",
+                )),
+            SizedBox(
+              width: Responsive.isDesktop(context)
+                  ? 410
+                  : MediaQuery.of(context).size.width,
+              child: tile2(tileHeading: "Total Assignments", tileData: "7"),
+            ),
+          ],
+        ),
+        searchBar(
+          title: "Search for Assignments",
+          opt1: searchInputOptions(title: "Teacher", opt1: ""),
+          opt2: searchInputOptions(title: "Department", opt1: ""),
+          opt3: searchInputOptions(title: "Class", opt1: ""),
+          opt4: searchInputOptions(title: "Stream", opt1: ""),
+          opt5: searchInputOptions(title: "Subject", opt1: ""),
+        ),
+        downloadBar(results: "7"),
+        Expanded(
+          child: ListView(children: [
+            Container(
+              margin: EdgeInsets.only(
+                left: Responsive.isDesktop(context) ? Insets().appPadding : 13,
+                right: Responsive.isDesktop(context) ? Insets().appPadding : 13,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(Insets().appGap + 4)),
+              padding: EdgeInsets.only(
+                  left: 15, right: 15, bottom: Insets().appPadding),
+              child: Center(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                      headingTextStyle:
+                          TextStyle(color: Palette().primaryColor),
+                      horizontalMargin: 0,
+                      columnSpacing: Responsive.isDesktop(context) ? 20 : 10,
+                      columns: [
+                        DataColumn(
+                            label: Checkbox(
+                          value: false,
+                          onChanged: (value) {
+                            setState(() {});
+                          },
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          child: HeadingText(
+                            size: 14,
+                            value: "No.",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 140 : 50,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Title",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 140 : 65,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Teacher",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 120 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Class",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 100 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Term",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 80 : 80,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Section",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 100 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Subject",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 150 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Date of Submission",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 100 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Action",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? null : null,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Viewed",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                      ],
+                      rows: []),
+                ),
+              ),
+            ),
+          ]),
+        )
+      ])),
+    );
+  }
+}
