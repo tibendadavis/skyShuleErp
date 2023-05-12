edcf1fc884aec5a21a5703f60693b13832dc8370
Dave799 <davistibz@gmail.com>
2023-05-12 17:08:19 +0300

    🚀 Organized file structure

--- lib/screens/Messages/inbox.scrn.dart
+++ lib/screens/Messages/inbox.scrn.dart
@@ -13,10 +13,10 @@ import 'package:skyconnect_starter/components/heading6.dart';
 import 'package:skyconnect_starter/components/heading_text.dart';
 import 'package:skyconnect_starter/controllers/responsive.dart';
 import 'package:skyconnect_starter/main.dart';
-import 'package:skyconnect_starter/screens/addSubject.scrn.dart';
-import 'package:skyconnect_starter/screens/addTerm.scrn.dart';
+import 'package:skyconnect_starter/screens/Subjects/addSubject.scrn.dart';
+import 'package:skyconnect_starter/screens/Terms/addTerm.scrn.dart';
 import 'package:skyconnect_starter/screens/Messages/compose.scrn.dart';
-import 'package:skyconnect_starter/screens/student_admission.scrn.dart';
+import 'package:skyconnect_starter/screens/Users/student_admission.scrn.dart';
 import 'package:skyconnect_starter/theme/design.theme.dart';
 
 class inbox extends StatefulWidget {
