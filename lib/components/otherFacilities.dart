d7f3b1aa0f0b690fffc0219204327f214e9594ec
Dave799 <davistibz@gmail.com>
2023-05-12 13:50:25 +0300

    🚀 Organized file structure

--- lib/components/otherFacilities.dart
+++ lib/components/otherFacilities.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:image/image.dart';
+
 import 'package:skyconnect_starter/components/heading4.dart';
 import 'package:skyconnect_starter/components/heading_text.dart';
 import 'package:skyconnect_starter/controllers/responsive.dart';
