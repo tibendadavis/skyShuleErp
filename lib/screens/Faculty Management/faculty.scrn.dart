c63d020f2b0378ac0390a2121a38b63ebdd31e3d
Dave799 <davistibz@gmail.com>
2023-05-12 17:04:07 +0300

    🚀 Organized file structure

--- lib/screens/Faculty Management/faculty.scrn.dart
+++ lib/screens/Faculty Management/faculty.scrn.dart
@@ -12,13 +12,13 @@ import 'package:skyconnect_starter/components/heading5.dart';
 import 'package:skyconnect_starter/components/heading6.dart';
 import 'package:skyconnect_starter/components/heading_text.dart';
 import 'package:skyconnect_starter/controllers/responsive.dart';
-import 'package:skyconnect_starter/screens/addEmployeeSalary.scrn.dart';
-import 'package:skyconnect_starter/screens/addFaculty.scrn.dart';
-import 'package:skyconnect_starter/screens/addFeePayment.scrn.dart';
-import 'package:skyconnect_starter/screens/addPayroll.scrn.dart';
-import 'package:skyconnect_starter/screens/addSubject.scrn.dart';
-import 'package:skyconnect_starter/screens/addTerm.scrn.dart';
-import 'package:skyconnect_starter/screens/student_admission.scrn.dart';
+import 'package:skyconnect_starter/screens/Payroll%20Management/addEmployeeSalary.scrn.dart';
+import 'package:skyconnect_starter/screens/Faculty%20Management/addFaculty.scrn.dart';
+import 'package:skyconnect_starter/screens/Finance%20Management/addFeePayment.scrn.dart';
+import 'package:skyconnect_starter/screens/Payroll%20Management/addPayroll.scrn.dart';
+import 'package:skyconnect_starter/screens/Subjects/addSubject.scrn.dart';
+import 'package:skyconnect_starter/screens/Terms/addTerm.scrn.dart';
+import 'package:skyconnect_starter/screens/Users/student_admission.scrn.dart';
 import 'package:skyconnect_starter/theme/design.theme.dart';
 
 class faculty extends StatefulWidget {
