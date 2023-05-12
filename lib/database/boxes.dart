c201c8a83f0e5b4df517644818cc9633e6765bdb
Dave799 <davistibz@gmail.com>
2023-05-12 13:51:37 +0300

    🚀 Organized file structure

--- lib/database/boxes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:skyconnect_starter/database/db.dart';
-import 'package:hive/hive.dart';
-import 'package:skyconnect_starter/database/db.dart';
-
-class Boxes {
-  static Box<MySetting> getMySettings() => Hive.box<MySetting>("mySettings");
-
-  static Box<Filter> getFilters() => Hive.box<Filter>("filters");
-}
