3de58daeca1c8805ce4b2ed66f354a96dd0a7d83
Dave799 <davistibz@gmail.com>
2023-05-12 13:59:56 +0300

    🚀 Organized file structure

---
+++ lib/screens/Finance Management/accountManagement.scrn.dart
@@ -0,0 +1,190 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
+import 'package:path/path.dart';
+import 'package:skyconnect_starter/components/app_drawer/skyShuleDrawer.dart';
+import 'package:skyconnect_starter/components/downloadBar.comp.dart';
+import 'package:skyconnect_starter/components/header.dart';
+import 'package:skyconnect_starter/components/heading1.dart';
+import 'package:skyconnect_starter/components/heading2.dart';
+import 'package:skyconnect_starter/components/heading3.dart';
+import 'package:skyconnect_starter/components/heading4.dart';
+import 'package:skyconnect_starter/components/heading5.dart';
+import 'package:skyconnect_starter/components/heading6.dart';
+import 'package:skyconnect_starter/components/heading_text.dart';
+import 'package:skyconnect_starter/components/searchBar.comp.dart';
+import 'package:skyconnect_starter/components/searchInputOptions.comp.dart';
+import 'package:skyconnect_starter/components/tile2.comp.dart';
+import 'package:skyconnect_starter/components/tile3.comp.dart';
+import 'package:skyconnect_starter/controllers/funcs_main.dart';
+import 'package:skyconnect_starter/controllers/responsive.dart';
+import 'package:skyconnect_starter/screens/addAccount.scrn.dart';
+import 'package:skyconnect_starter/screens/addEmployeeLeave.scrn..dart';
+import 'package:skyconnect_starter/screens/addEmployeeSalary.scrn.dart';
+import 'package:skyconnect_starter/screens/addFeeCategory.scrn.dart';
+import 'package:skyconnect_starter/screens/addSubject.scrn.dart';
+import 'package:skyconnect_starter/screens/addTerm.scrn.dart';
+import 'package:skyconnect_starter/screens/student_admission.scrn.dart';
+import 'package:skyconnect_starter/theme/design.theme.dart';
+
+class accountManagement extends StatefulWidget {
+  const accountManagement({super.key});
+
+  @override
+  State<accountManagement> createState() => _accountManagementState();
+}
+
+class _accountManagementState extends State<accountManagement> {
+  bool _menu = false;
+  double _drawersize = 250;
+  TextEditingController _From = TextEditingController();
+  TextEditingController _To = TextEditingController();
+  var _classlevel;
+  var _academicYear;
+  var _stream;
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      child: Expanded(
+          child: Column(children: [
+        Container(
+          alignment: Alignment.bottomLeft,
+          padding: EdgeInsets.only(
+              top: Insets().appPadding,
+              left: Responsive.isDesktop(context)
+                  ? Insets().appPadding * 2
+                  : Insets().appPadding,
+              right: Insets().appGap),
+          child: HeadingText(
+              size: Responsive.isDesktop(context) ? 35 : 30,
+              value: "Account Management",
+              fontWeight: FontWeight.w700,
+              color: Colors.grey.shade800),
+        ),
+        Flex(
+          direction:
+              Responsive.isDesktop(context) ? Axis.horizontal : Axis.vertical,
+          children: [
+            SizedBox(
+                width: Responsive.isDesktop(context)
+                    ? 410
+                    : MediaQuery.of(context).size.width,
+                child: tile3(
+                  icon: Icons.manage_accounts_rounded,
+                  link: addAccount(),
+                  linkTitle: "Add Account",
+                )),
+            SizedBox(
+              width: Responsive.isDesktop(context)
+                  ? 410
+                  : MediaQuery.of(context).size.width,
+              child: tile2(tileHeading: "Total Accounts", tileData: "7"),
+            ),
+          ],
+        ),
+        searchBar(
+            title: "Search for Account",
+            opt1: searchInputOptions(
+              title: "Account Type",
+              opt1: "Student",
+              opt2: "Teaching Staff",
+              opt3: "Non-Teaching Staff",
+            )),
+        downloadBar(results: "7"),
+        Expanded(
+          child: ListView(children: [
+            Container(
+              margin: EdgeInsets.only(
+                left: Responsive.isDesktop(context) ? Insets().appPadding : 13,
+                right: Responsive.isDesktop(context) ? Insets().appPadding : 13,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(Insets().appGap + 4)),
+              padding: EdgeInsets.only(
+                  left: 15, right: 15, bottom: Insets().appPadding),
+              child: Center(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                      headingTextStyle:
+                          TextStyle(color: Palette().primaryColor),
+                      horizontalMargin: 0,
+                      columnSpacing: Responsive.isDesktop(context) ? 20 : 10,
+                      columns: [
+                        DataColumn(
+                            label: Checkbox(
+                          value: false,
+                          onChanged: (value) {
+                            setState(() {});
+                          },
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 60 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "No.",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 180 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Account No.",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 160 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Account Type",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 220 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Account Description",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 120 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Receipts",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                        DataColumn(
+                            label: SizedBox(
+                          width: Responsive.isDesktop(context) ? 220 : 100,
+                          child: HeadingText(
+                            size: 14,
+                            value: "Action",
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )),
+                      ],
+                      rows: []),
+                ),
+              ),
+            ),
+          ]),
+        )
+      ])),
+    );
+  }
+}
