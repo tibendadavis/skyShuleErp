0cb9b6f3fceaca0b2a9605d3ee266ac9d56e3ecd
Dave799 <davistibz@gmail.com>
2023-05-12 17:01:21 +0300

    🚀 Organized file structure

--- lib/screens/Attendance/attendanceReport.scrn.dart
+++ lib/screens/Attendance/attendanceReport.scrn.dart
@@ -14,9 +14,9 @@ import 'package:skyconnect_starter/components/heading6.dart';
 import 'package:skyconnect_starter/components/heading_text.dart';
 import 'package:skyconnect_starter/controllers/funcs_main.dart';
 import 'package:skyconnect_starter/controllers/responsive.dart';
-import 'package:skyconnect_starter/screens/addSubject.scrn.dart';
-import 'package:skyconnect_starter/screens/addTerm.scrn.dart';
-import 'package:skyconnect_starter/screens/student_admission.scrn.dart';
+import 'package:skyconnect_starter/screens/Subjects/addSubject.scrn.dart';
+import 'package:skyconnect_starter/screens/Terms/addTerm.scrn.dart';
+import 'package:skyconnect_starter/screens/Users/student_admission.scrn.dart';
 import 'package:skyconnect_starter/theme/design.theme.dart';
 
 class attendanceReport extends StatefulWidget {
