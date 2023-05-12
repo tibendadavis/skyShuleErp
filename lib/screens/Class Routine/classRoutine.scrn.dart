290712886e12d37dbfd8db5df802ec90e0a51a32
Dave799 <davistibz@gmail.com>
2023-05-12 17:04:21 +0300

    🚀 Organized file structure

--- lib/screens/Class Routine/classRoutine.scrn.dart
+++ lib/screens/Class Routine/classRoutine.scrn.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -17,8 +16,8 @@ import 'package:skyconnect_starter/components/searchBar.comp.dart';
 import 'package:skyconnect_starter/components/searchInputOptions.comp.dart';
 import 'package:skyconnect_starter/components/tile3.comp.dart';
 import 'package:skyconnect_starter/controllers/responsive.dart';
-import 'package:skyconnect_starter/screens/addClassRoutine.scrn.dart';
-import 'package:skyconnect_starter/screens/student_admission.scrn.dart';
+import 'package:skyconnect_starter/screens/Class%20Routine/addClassRoutine.scrn.dart';
+import 'package:skyconnect_starter/screens/Users/student_admission.scrn.dart';
 import 'package:skyconnect_starter/theme/design.theme.dart';
 
 class classRoutine extends StatefulWidget {
