0120a8f15f9f98ba0f4c3e58cb13a2213844664a
Dave799 <davistibz@gmail.com>
2023-05-15 19:08:24 +0300

    🚀 Updated system UI

--- lib/screens/eResources/onlineDiscussion.scrn.dart
+++ lib/screens/eResources/onlineDiscussion.scrn.dart
@@ -11,6 +11,8 @@ import 'package:skyconnect_starter/components/heading4.dart';
 import 'package:skyconnect_starter/components/heading5.dart';
 import 'package:skyconnect_starter/components/heading6.dart';
 import 'package:skyconnect_starter/components/heading_text.dart';
+import 'package:skyconnect_starter/components/searchBar.comp.dart';
+import 'package:skyconnect_starter/components/tile3.comp.dart';
 import 'package:skyconnect_starter/controllers/responsive.dart';
 import 'package:skyconnect_starter/screens/Subjects/addSubject.scrn.dart';
 import 'package:skyconnect_starter/screens/Terms/addTerm.scrn.dart';
@@ -47,9 +49,9 @@ class _onlineDiscussionState extends State<onlineDiscussion> {
               right: Insets().appGap),
           child: HeadingText(
             size: Responsive.isDesktop(context) ? 35 : 30,
-            value: "ONLINE DISCUSSION",
+            value: "Online Discussion",
             fontWeight: FontWeight.w700,
-            color: Colors.black,
+            color: Colors.grey.shade800,
           ),
         ),
         Flex(
@@ -57,333 +59,27 @@ class _onlineDiscussionState extends State<onlineDiscussion> {
               Responsive.isDesktop(context) ? Axis.horizontal : Axis.vertical,
           children: [
             SizedBox(
-              width: Responsive.isDesktop(context)
-                  ? 410
-                  : MediaQuery.of(context).size.width,
-              child: Container(
-                margin: EdgeInsets.only(
-                    left: Responsive.isDesktop(context)
-                        ? Insets().appPadding * 2
-                        : Insets().appPadding,
-                    right:
-                        Responsive.isDesktop(context) ? 0 : Insets().appPadding,
-                    top: Responsive.isDesktop(context)
-                        ? Insets().appPadding
-                        : 12,
-                    bottom: Responsive.isDesktop(context)
-                        ? Insets().appPadding
-                        : 0),
-                padding: EdgeInsets.only(
-                    left: Insets().appPadding,
-                    right: Insets().appPadding,
-                    top: Insets().appGap + 2,
-                    bottom: Insets().appPadding),
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Palette().borderColor,
-                        blurRadius: 15.0, // soften the shadow
-                        spreadRadius: 2.0, //extend the shadow
-                        offset: Offset(
-                          1.0, // Move to right 5  horizontally
-                          2.0, // Move to bottom 5 Vertically
-                        ),
-                      )
-                    ],
-                    color: Palette().primaryColor,
-                    borderRadius:
-                        BorderRadius.circular(Insets().appRadiusMin + 4)),
-                child: SizedBox(
-                  height: 70,
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.question_mark_rounded,
-                        size: 50,
-                        color: Colors.white,
-                      ),
-                      Spacer(),
-                      ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(
-                                      Insets().appRadiusMin + 4)),
-                              padding: EdgeInsets.all(Insets().appPadding)),
-                          child: Heading5(
-                            value: "Ask a Question",
-                            color: Colors.black,
-                          ))
-                    ],
-                  ),
-                ),
-              ),
-            ),
+                width: Responsive.isDesktop(context)
+                    ? 360
+                    : MediaQuery.of(context).size.width,
+                child: tile3(
+                  icon: Icons.question_answer,
+                  link: null,
+                  linkTitle: "Ask a Question",
+                )),
             SizedBox(
-              width: Responsive.isDesktop(context)
-                  ? 410
-                  : MediaQuery.of(context).size.width,
-              child: Container(
-                margin: EdgeInsets.only(
-                    left: Responsive.isDesktop(context)
-                        ? Insets().appPadding * 2
-                        : Insets().appPadding,
-                    right:
-                        Responsive.isDesktop(context) ? 0 : Insets().appPadding,
-                    top: Responsive.isDesktop(context)
-                        ? Insets().appPadding
-                        : 12,
-                    bottom: Responsive.isDesktop(context)
-                        ? Insets().appPadding
-                        : 0),
-                padding: EdgeInsets.only(
-                    left: Insets().appPadding,
-                    right: Insets().appPadding,
-                    top: Insets().appGap + 2,
-                    bottom: Insets().appPadding),
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Palette().borderColor,
-                        blurRadius: 15.0, // soften the shadow
-                        spreadRadius: 2.0, //extend the shadow
-                        offset: Offset(
-                          1.0, // Move to right 5  horizontally
-                          2.0, // Move to bottom 5 Vertically
-                        ),
-                      )
-                    ],
-                    color: Palette().primaryColor,
-                    borderRadius:
-                        BorderRadius.circular(Insets().appRadiusMin + 4)),
-                child: SizedBox(
-                  height: 70,
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.class_,
-                        size: 50,
-                        color: Colors.white,
-                      ),
-                      Spacer(),
-                      ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(
-                                      Insets().appRadiusMin + 4)),
-                              padding: EdgeInsets.all(Insets().appPadding)),
-                          child: Heading5(
-                            value: "Class Discussion",
-                            color: Colors.black,
-                          ))
-                    ],
-                  ),
-                ),
-              ),
-            ),
+                width: Responsive.isDesktop(context)
+                    ? 360
+                    : MediaQuery.of(context).size.width,
+                child: tile3(
+                  icon: Icons.class_rounded,
+                  link: null,
+                  linkTitle: "Class Discussion",
+                )),
           ],
         ),
-        Container(
-          margin: EdgeInsets.only(
-              left: Responsive.isDesktop(context)
-                  ? Insets().appPadding * 2
-                  : Insets().appPadding,
-              right: Responsive.isDesktop(context)
-                  ? Insets().appPadding * 2
-                  : Insets().appPadding,
-              top: Responsive.isDesktop(context) ? Insets().appPadding : 12,
-              bottom: Responsive.isDesktop(context) ? Insets().appPadding : 12),
-          padding: EdgeInsets.only(
-              left: Responsive.isDesktop(context) ? Insets().appPadding : 10,
-              right: Responsive.isDesktop(context) ? Insets().appGap / 2 : 10,
-              top: Responsive.isDesktop(context) ? Insets().appGap / 3 : 5,
-              bottom: Responsive.isDesktop(context) ? Insets().appGap / 3 : 10),
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: 2),
-              color: Palette().primaryColorLight,
-              borderRadius: BorderRadius.circular(Insets().appGap + 4)),
-          child: Flex(
-              direction: Responsive.isDesktop(context)
-                  ? Axis.horizontal
-                  : Axis.vertical,
-              crossAxisAlignment: Responsive.isDesktop(context)
-                  ? CrossAxisAlignment.center
-                  : CrossAxisAlignment.start,
-              children: [
-                if (Responsive.isDesktop(context)) ...[
-                  Expanded(
-                      flex: 3,
-                      child: TextFormField(
-                          textAlignVertical: TextAlignVertical.center,
-                          decoration: const InputDecoration(
-
-                              // enabledBorder: OutlineInputBorder(
-                              //     borderSide: BorderSide(
-                              //         color: Palette().borderColor, width: 3.0)),
-                              border: InputBorder.none,
-                              hintText: " Search ",
-                              hintStyle: TextStyle(fontSize: 20)))),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Expanded(
-                      flex: 1,
-                      child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Palette().primaryColor,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(
-                                      Insets().appRadiusMin + 4)),
-                              padding:
-                                  EdgeInsets.all(Insets().appPadding / 1.5)),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.check,
-                                color: Colors.white,
-                                size: 30,
-                              ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Heading6(
-                                value: "Apply",
-                                color: Colors.white,
-                              ),
-                            ],
-                          ))),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Expanded(
-                      flex: 1,
-                      child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Palette().primaryColor,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(
-                                      Insets().appRadiusMin + 4)),
-                              padding:
-                                  EdgeInsets.all(Insets().appPadding / 1.5)),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.restart_alt_rounded,
-                                color: Colors.white,
-                                size: 30,
-                              ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Heading6(
-                                value: "Clear",
-                                color: Colors.white,
-                              ),
-                            ],
-                          )))
-                ],
-                if (!Responsive.isDesktop(context)) ...[
-                  Container(
-                      child: TextFormField(
-                          textAlignVertical: TextAlignVertical.center,
-                          decoration: const InputDecoration(
-
-                              // enabledBorder: OutlineInputBorder(
-                              //     borderSide: BorderSide(
-                              //         color: Palette().borderColor, width: 3.0)),
-                              border: InputBorder.none,
-                              hintText: " Search ",
-                              hintStyle: TextStyle(fontSize: 20)))),
-                  SizedBox(
-                    width: Responsive.isDesktop(context) ? 10 : 0,
-                    height: Responsive.isDesktop(context) ? 0 : 5,
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: SizedBox(
-                        width: MediaQuery.of(context).size.width,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [],
-                        )),
-                  ),
-                  SizedBox(
-                    width: Responsive.isDesktop(context) ? 10 : 0,
-                    height: Responsive.isDesktop(context) ? 0 : 0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                          width: MediaQuery.of(context).size.width / 2.5,
-                          child: ElevatedButton(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: Palette().primaryColor,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(
-                                          Insets().appRadiusMin + 4)),
-                                  padding: EdgeInsets.all(
-                                      Insets().appPadding / 1.5)),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.check,
-                                    color: Colors.white,
-                                    size: 30,
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Heading6(
-                                    value: "Apply",
-                                    color: Colors.white,
-                                  ),
-                                ],
-                              ))),
-                      SizedBox(
-                        width: Responsive.isDesktop(context) ? 10 : 0,
-                      ),
-                      Container(
-                          width: MediaQuery.of(context).size.width / 2.5,
-                          child: ElevatedButton(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: Palette().primaryColor,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(
-                                          Insets().appRadiusMin + 4)),
-                                  padding: EdgeInsets.all(
-                                      Insets().appPadding / 1.5)),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.restart_alt_rounded,
-                                    color: Colors.white,
-                                    size: 30,
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Heading6(
-                                    value: "Clear",
-                                    color: Colors.white,
-                                  ),
-                                ],
-                              )))
-                    ],
-                  ),
-                ]
-              ]),
+        searchBar(
+          title: "Search",
         ),
         Container(
           margin: EdgeInsets.only(
@@ -419,7 +115,7 @@ class _onlineDiscussionState extends State<onlineDiscussion> {
                           ? Insets().appPadding
                           : 15),
                   decoration: BoxDecoration(
-                      color: Palette().primaryColorLight,
+                      color: Colors.white,
                       borderRadius: BorderRadius.circular(Insets().appGap + 4)),
                   child: SizedBox(
                     height: 500,
@@ -1699,7 +1395,7 @@ class _onlineDiscussionState extends State<onlineDiscussion> {
                                 ? Insets().appPadding / 2
                                 : 15),
                         decoration: BoxDecoration(
-                            color: Palette().primaryColorLight,
+                            color: Colors.white,
                             borderRadius:
                                 BorderRadius.circular(Insets().appGap + 4)),
                         child: SizedBox(
