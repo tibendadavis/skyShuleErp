0a73b64d6143a3661dac9273ebb848121c97ebe2
Dave799 <davistibz@gmail.com>
2023-05-26 17:04:03 +0300

    🚀 updated home page

--- lib/components/searchInputDate.comp.dart
+++ lib/components/searchInputDate.comp.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:skyconnect_starter/controllers/funcs_main.dart';
 import 'package:skyconnect_starter/theme/design.theme.dart';
 import 'package:intl/intl.dart';
