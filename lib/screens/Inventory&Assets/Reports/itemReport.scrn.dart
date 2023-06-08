8cc970424f95aba800b90ad3a268a45bf6387cc2
Dave799 <davistibz@gmail.com>
2023-06-08 18:36:56 +0300

    🚀 Updated table UI

--- lib/screens/Inventory&Assets/Reports/itemReport.scrn.dart
+++ lib/screens/Inventory&Assets/Reports/itemReport.scrn.dart
@@ -1,29 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:intl/intl.dart';
-import 'package:path/path.dart';
-import 'package:skyconnect_starter/components/app_drawer/skyShuleDrawer.dart';
 import 'package:skyconnect_starter/components/downloadBar.comp.dart';
-import 'package:skyconnect_starter/components/header.dart';
-import 'package:skyconnect_starter/components/heading1.dart';
-import 'package:skyconnect_starter/components/heading2.dart';
-import 'package:skyconnect_starter/components/heading3.dart';
-import 'package:skyconnect_starter/components/heading4.dart';
-import 'package:skyconnect_starter/components/heading5.dart';
-import 'package:skyconnect_starter/components/heading6.dart';
 import 'package:skyconnect_starter/components/heading_text.dart';
 import 'package:skyconnect_starter/components/searchBar.comp.dart';
 import 'package:skyconnect_starter/components/searchInputDate.comp.dart';
 import 'package:skyconnect_starter/components/searchInputOptions.comp.dart';
 import 'package:skyconnect_starter/components/tile2.comp.dart';
-import 'package:skyconnect_starter/controllers/funcs_main.dart';
 import 'package:skyconnect_starter/controllers/responsive.dart';
-import 'package:skyconnect_starter/screens/Leave%20Management/addEmployeeLeave.scrn..dart';
-import 'package:skyconnect_starter/screens/Payroll%20Management/addEmployeeSalary.scrn.dart';
-import 'package:skyconnect_starter/screens/Subjects/addSubject.scrn.dart';
-import 'package:skyconnect_starter/screens/Terms/addTerm.scrn.dart';
-import 'package:skyconnect_starter/screens/Users/student_admission.scrn.dart';
 import 'package:skyconnect_starter/theme/design.theme.dart';
 
 class itemReport extends StatefulWidget {
@@ -101,132 +83,101 @@ class _itemReportState extends State<itemReport> {
                   borderRadius: BorderRadius.circular(Insets().appGap + 4)),
               padding: EdgeInsets.only(
                   left: 15, right: 15, bottom: Insets().appPadding),
-              child: Center(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: DataTable(
-                    headingTextStyle: TextStyle(color: Palette().primaryColor),
-                    horizontalMargin: 0,
-                    columnSpacing: Responsive.isDesktop(context) ? 20 : 10,
-                    columns: [
-                      DataColumn(
-                          label: Checkbox(
-                        value: false,
-                        onChanged: (value) {
-                          setState(() {});
-                        },
-                      )),
-                      DataColumn(
-                          label: SizedBox(
-                        width: Responsive.isDesktop(context) ? null : null,
-                        child: HeadingText(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                  dataRowHeight: 55,
+                  headingTextStyle: TextStyle(color: Palette().primaryColor),
+                  horizontalMargin: 0,
+                  columnSpacing:
+                      Responsive.isDesktop(context) && size.width < 1600
+                          ? size.width / 65
+                          : Responsive.isDesktop(context) && size.width > 1600
+                              ? size.width / 40
+                              : 25,
+                  showCheckboxColumn: true,
+                  checkboxHorizontalMargin: 10,
+                  showBottomBorder: true,
+                  columns: [
+                    DataColumn(
+                        label: Checkbox(
+                      value: false,
+                      onChanged: (value) {
+                        setState(() {});
+                      },
+                    )),
+                    DataColumn(
+                        label: HeadingText(
+                      size: 14,
+                      value: "No.",
+                      fontWeight: FontWeight.w700,
+                    )),
+                    DataColumn(
+                      label: HeadingText(
                           size: 14,
-                          value: "No.",
                           fontWeight: FontWeight.w700,
-                        ),
-                      )),
-                      DataColumn(
-                        label: SizedBox(
-                          width: Responsive.isDesktop(context) ? 150 : 100,
-                          child: HeadingText(
-                              size: 14,
-                              fontWeight: FontWeight.w700,
-                              value: 'Item Name'),
-                        ),
-                      ),
-                      DataColumn(
-                        label: SizedBox(
-                          width: Responsive.isDesktop(context) ? 60 : 60,
-                          child: HeadingText(
-                              size: 14,
-                              fontWeight: FontWeight.w700,
-                              value: 'Item ID'),
-                        ),
-                      ),
-                      DataColumn(
-                        label: SizedBox(
-                          width: Responsive.isDesktop(context) ? 100 : 100,
-                          child: HeadingText(
-                              size: 14,
-                              fontWeight: FontWeight.w700,
-                              value: 'Category'),
-                        ),
-                      ),
-                      DataColumn(
-                        label: SizedBox(
-                          width: Responsive.isDesktop(context) ? 120 : 100,
-                          child: HeadingText(
-                              size: 14,
-                              fontWeight: FontWeight.w700,
-                              value: 'Location'),
-                        ),
-                      ),
-                      DataColumn(
-                        label: SizedBox(
-                          width: Responsive.isDesktop(context) ? null : null,
-                          child: HeadingText(
-                              size: 14,
-                              fontWeight: FontWeight.w700,
-                              value: 'Quantity'),
-                        ),
-                      ),
-                      DataColumn(
-                        label: SizedBox(
-                          width: Responsive.isDesktop(context) ? null : null,
-                          child: HeadingText(
-                              fontWeight: FontWeight.w700,
-                              size: 14,
-                              value: 'Reorder Level'),
-                        ),
-                      ),
-                      DataColumn(
-                        label: SizedBox(
-                          width: Responsive.isDesktop(context) ? null : null,
-                          child: HeadingText(
-                              fontWeight: FontWeight.w700,
-                              size: 14,
-                              value: 'Reorder Quantity'),
-                        ),
-                      ),
-                      DataColumn(
-                        label: SizedBox(
-                          width: Responsive.isDesktop(context) ? 120 : 120,
-                          child: HeadingText(
-                              size: 14,
-                              fontWeight: FontWeight.w700,
-                              value: 'Cost'),
-                        ),
-                      ),
-                      DataColumn(
-                        label: SizedBox(
-                          width: Responsive.isDesktop(context) ? null : null,
-                          child: HeadingText(
-                              fontWeight: FontWeight.w700,
-                              size: 14,
-                              value: 'Last  Purchase\nPrice'),
-                        ),
-                      ),
-                      DataColumn(
-                        label: SizedBox(
-                          width: Responsive.isDesktop(context) ? null : null,
-                          child: HeadingText(
-                              fontWeight: FontWeight.w700,
-                              size: 14,
-                              value: 'Last  Purchase\nDate'),
-                        ),
-                      ),
-                      DataColumn(
-                        label: SizedBox(
-                          width: Responsive.isDesktop(context) ? 100 : 100,
-                          child: HeadingText(
-                              size: 14,
-                              fontWeight: FontWeight.w700,
-                              value: 'Action'),
-                        ),
-                      ),
-                    ],
-                    rows: [],
-                  ),
+                          value: 'Item Name'),
+                    ),
+                    DataColumn(
+                      label: HeadingText(
+                          size: 14,
+                          fontWeight: FontWeight.w700,
+                          value: 'Item ID'),
+                    ),
+                    DataColumn(
+                      label: HeadingText(
+                          size: 14,
+                          fontWeight: FontWeight.w700,
+                          value: 'Category'),
+                    ),
+                    DataColumn(
+                      label: HeadingText(
+                          size: 14,
+                          fontWeight: FontWeight.w700,
+                          value: 'Location'),
+                    ),
+                    DataColumn(
+                      label: HeadingText(
+                          size: 14,
+                          fontWeight: FontWeight.w700,
+                          value: 'Quantity'),
+                    ),
+                    DataColumn(
+                      label: HeadingText(
+                          fontWeight: FontWeight.w700,
+                          size: 14,
+                          value: 'Reorder Level'),
+                    ),
+                    DataColumn(
+                      label: HeadingText(
+                          fontWeight: FontWeight.w700,
+                          size: 14,
+                          value: 'Reorder Quantity'),
+                    ),
+                    DataColumn(
+                      label: HeadingText(
+                          size: 14, fontWeight: FontWeight.w700, value: 'Cost'),
+                    ),
+                    DataColumn(
+                      label: HeadingText(
+                          fontWeight: FontWeight.w700,
+                          size: 14,
+                          value: 'Last  Purchase\nPrice'),
+                    ),
+                    DataColumn(
+                      label: HeadingText(
+                          fontWeight: FontWeight.w700,
+                          size: 14,
+                          value: 'Last  Purchase\nDate'),
+                    ),
+                    DataColumn(
+                      label: HeadingText(
+                          size: 14,
+                          fontWeight: FontWeight.w700,
+                          value: 'Action'),
+                    ),
+                  ],
+                  rows: [],
                 ),
               ),
             ),
