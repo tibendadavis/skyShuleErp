5857d9f217ef0b1a0b34f0b15472a225306ae9ee
Dave799 <davistibz@gmail.com>
2023-06-09 18:42:13 +0300

    🚀 updated Table UI

--- lib/screens/Terms/terms.scrn.dart
+++ lib/screens/Terms/terms.scrn.dart
@@ -1,25 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:path/path.dart';
-import 'package:skyconnect_starter/components/app_drawer/skyShuleDrawer.dart';
 import 'package:skyconnect_starter/components/downloadBar.comp.dart';
-import 'package:skyconnect_starter/components/header.dart';
-import 'package:skyconnect_starter/components/heading1.dart';
-import 'package:skyconnect_starter/components/heading2.dart';
-import 'package:skyconnect_starter/components/heading3.dart';
-import 'package:skyconnect_starter/components/heading4.dart';
-import 'package:skyconnect_starter/components/heading5.dart';
-import 'package:skyconnect_starter/components/heading6.dart';
 import 'package:skyconnect_starter/components/heading_text.dart';
 import 'package:skyconnect_starter/components/searchBar.comp.dart';
 import 'package:skyconnect_starter/components/searchInputOptions.comp.dart';
 import 'package:skyconnect_starter/components/tile2.comp.dart';
 import 'package:skyconnect_starter/components/tile3.comp.dart';
 import 'package:skyconnect_starter/controllers/responsive.dart';
-import 'package:skyconnect_starter/screens/Subjects/addSubject.scrn.dart';
 import 'package:skyconnect_starter/screens/Terms/addTerm.scrn.dart';
-import 'package:skyconnect_starter/screens/Users/student_admission.scrn.dart';
 import 'package:skyconnect_starter/theme/design.theme.dart';
 
 class terms extends StatefulWidget {
@@ -94,181 +81,153 @@ class _termsState extends State<terms> {
                   borderRadius: BorderRadius.circular(Insets().appGap + 4)),
               padding: EdgeInsets.only(
                   left: 15, right: 15, bottom: Insets().appPadding),
-              child: Center(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: DataTable(
-                      headingTextStyle:
-                          TextStyle(color: Palette().primaryColor),
-                      horizontalMargin: 0,
-                      columnSpacing: Responsive.isDesktop(context) ? 20 : 10,
-                      columns: [
-                        DataColumn(
-                            label: Checkbox(
-                          value: false,
-                          onChanged: (value) {
-                            setState(() {});
-                          },
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                    dataRowHeight: 55,
+                    headingTextStyle: TextStyle(color: Palette().primaryColor),
+                    horizontalMargin: 0,
+                    columnSpacing:
+                        Responsive.isDesktop(context) && size.width < 1600
+                            ? size.width / 14
+                            : Responsive.isDesktop(context) && size.width > 1600
+                                ? size.width / 10
+                                : 25,
+                    showCheckboxColumn: true,
+                    checkboxHorizontalMargin: 10,
+                    showBottomBorder: true,
+                    columns: [
+                      DataColumn(
+                          label: Expanded(
+                        child: HeadingText(
+                          size: 14,
+                          value: "No.",
+                          fontWeight: FontWeight.w700,
+                        ),
+                      )),
+                      DataColumn(
+                          label: HeadingText(
+                        size: 14,
+                        value: "Term",
+                        fontWeight: FontWeight.w700,
+                      )),
+                      DataColumn(
+                          label: HeadingText(
+                        size: 14,
+                        value: "Start Date",
+                        fontWeight: FontWeight.w700,
+                      )),
+                      DataColumn(
+                          label: HeadingText(
+                        size: 14,
+                        value: "End Date",
+                        fontWeight: FontWeight.w700,
+                      )),
+                      DataColumn(
+                          label: HeadingText(
+                        size: 14,
+                        value: "Study Days",
+                        fontWeight: FontWeight.w700,
+                      )),
+                      DataColumn(
+                          label: HeadingText(
+                        size: 14,
+                        value: "Action",
+                        fontWeight: FontWeight.w700,
+                      )),
+                    ],
+                    rows: [
+                      DataRow(cells: [
+                        DataCell(HeadingText(
+                          size: 14,
+                          value: "1",
                         )),
-                        DataColumn(
-                            label: Expanded(
-                          child: HeadingText(
-                            size: 14,
-                            value: "No.",
-                            fontWeight: FontWeight.w700,
-                          ),
+                        DataCell(HeadingText(
+                          size: 14,
+                          value: "FIRST TERM",
                         )),
-                        DataColumn(
-                            label: SizedBox(
-                          width: Responsive.isDesktop(context) ? 150 : 100,
-                          child: HeadingText(
-                            size: 14,
-                            value: "Term",
-                            fontWeight: FontWeight.w700,
-                          ),
+                        DataCell(HeadingText(
+                          size: 14,
+                          value: "2022 - 01 - 17",
                         )),
-                        DataColumn(
-                            label: SizedBox(
-                          width: Responsive.isDesktop(context) ? 150 : 100,
-                          child: HeadingText(
-                            size: 14,
-                            value: "Start Date",
-                            fontWeight: FontWeight.w700,
-                          ),
+                        DataCell(HeadingText(
+                          size: 14,
+                          value: "2022 - 06 - 24",
                         )),
-                        DataColumn(
-                            label: SizedBox(
-                          width: Responsive.isDesktop(context) ? 200 : 100,
-                          child: HeadingText(
-                            size: 14,
-                            value: "End Date",
-                            fontWeight: FontWeight.w700,
-                          ),
+                        DataCell(HeadingText(
+                          size: 14,
+                          value: "  ",
                         )),
-                        DataColumn(
-                            label: SizedBox(
-                          width: Responsive.isDesktop(context) ? 200 : 100,
-                          child: HeadingText(
-                            size: 14,
-                            value: "Study Days",
-                            fontWeight: FontWeight.w700,
-                          ),
-                        )),
-                        DataColumn(
-                            label: SizedBox(
-                          width: Responsive.isDesktop(context) ? 200 : 100,
-                          child: HeadingText(
-                            size: 14,
-                            value: "Action",
-                            fontWeight: FontWeight.w700,
-                          ),
-                        )),
-                      ],
-                      rows: [
-                        DataRow(cells: [
-                          DataCell(Checkbox(
-                            value: false,
-                            onChanged: (value) {
-                              setState(() {});
-                            },
-                          )),
-                          DataCell(HeadingText(
-                            size: 14,
-                            value: "1",
-                          )),
-                          DataCell(HeadingText(
-                            size: 14,
-                            value: "FIRST TERM",
-                          )),
-                          DataCell(HeadingText(
-                            size: 14,
-                            value: "2022 - 01 - 17",
-                          )),
-                          DataCell(HeadingText(
-                            size: 14,
-                            value: "2022 - 06 - 24",
-                          )),
-                          DataCell(HeadingText(
-                            size: 14,
-                            value: "  ",
-                          )),
-                          DataCell(Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              TextButton(
-                                onPressed: () {},
-                                child: HeadingText(
-                                  size: 14,
-                                  value: "Edit",
-                                ),
+                        DataCell(Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: HeadingText(
+                                size: 14,
+                                value: "Edit",
                               ),
-                              SizedBox(
-                                width: 5,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: HeadingText(
+                                size: 14,
+                                value: "Delete",
+                                color: Colors.red,
                               ),
-                              TextButton(
-                                onPressed: () {},
-                                child: HeadingText(
-                                  size: 14,
-                                  value: "Delete",
-                                  color: Colors.red,
-                                ),
-                              ),
-                            ],
-                          ))
-                        ]),
-                        DataRow(cells: [
-                          DataCell(Checkbox(
-                            value: false,
-                            onChanged: (value) {
-                              setState(() {});
-                            },
-                          )),
-                          DataCell(HeadingText(
-                            size: 14,
-                            value: "2",
-                          )),
-                          DataCell(HeadingText(
-                            size: 14,
-                            value: "SECOND TERM",
-                          )),
-                          DataCell(HeadingText(
-                            size: 14,
-                            value: "2022 - 01 - 17",
-                          )),
-                          DataCell(HeadingText(
-                            size: 14,
-                            value: "2022 - 06 - 24",
-                          )),
-                          DataCell(HeadingText(
-                            size: 14,
-                            value: "  ",
-                          )),
-                          DataCell(Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              TextButton(
-                                onPressed: () {},
-                                child: HeadingText(
-                                  size: 14,
-                                  value: "Edit",
-                                ),
-                              ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              TextButton(
-                                onPressed: () {},
-                                child: HeadingText(
-                                  size: 14,
-                                  value: "Delete",
-                                  color: Colors.red,
-                                ),
-                              ),
-                            ],
-                          ))
-                        ]),
+                            ),
+                          ],
+                        ))
                       ]),
-                ),
+                      DataRow(cells: [
+                        DataCell(HeadingText(
+                          size: 14,
+                          value: "2",
+                        )),
+                        DataCell(HeadingText(
+                          size: 14,
+                          value: "SECOND TERM",
+                        )),
+                        DataCell(HeadingText(
+                          size: 14,
+                          value: "2022 - 01 - 17",
+                        )),
+                        DataCell(HeadingText(
+                          size: 14,
+                          value: "2022 - 06 - 24",
+                        )),
+                        DataCell(HeadingText(
+                          size: 14,
+                          value: "  ",
+                        )),
+                        DataCell(Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: HeadingText(
+                                size: 14,
+                                value: "Edit",
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: HeadingText(
+                                size: 14,
+                                value: "Delete",
+                                color: Colors.red,
+                              ),
+                            ),
+                          ],
+                        ))
+                      ]),
+                    ]),
               ),
             ),
           ]),
