f57d529e8e77228f22e9047511594f9640a4de4a
Dave799 <davistibz@gmail.com>
2023-06-09 18:41:33 +0300

    🚀 updated Table UI

--- lib/screens/Settings/addInstitutionAdmin.scrn.dart
+++ lib/screens/Settings/addInstitutionAdmin.scrn.dart
@@ -1,29 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:intl/intl.dart';
-import 'package:path/path.dart';
-import 'package:skyconnect_starter/components/app_drawer/skyShuleDrawer.dart';
-import 'package:skyconnect_starter/components/bankDetails.dart';
-import 'package:skyconnect_starter/components/contactsDetails.dart';
-import 'package:skyconnect_starter/components/header.dart';
 import 'package:skyconnect_starter/components/heading2.dart';
 import 'package:skyconnect_starter/components/heading3.dart';
 import 'package:skyconnect_starter/components/heading_text.dart';
-import 'package:skyconnect_starter/components/heading5.dart';
-import 'package:skyconnect_starter/components/heading6.dart';
-import 'package:skyconnect_starter/components/heading_text.dart';
 import 'package:skyconnect_starter/components/inputOptions.comp.dart';
 import 'package:skyconnect_starter/components/inputTextField.comp.dart';
-import 'package:skyconnect_starter/components/officialDetails.dart';
-import 'package:skyconnect_starter/components/otherFacilities.dart';
-import 'package:skyconnect_starter/components/parentsDetails.dart';
-import 'package:skyconnect_starter/components/personalDetails.dart';
-import 'package:skyconnect_starter/components/uploadDocuments.dart';
-import 'package:skyconnect_starter/controllers/funcs_main.dart';
 import 'package:skyconnect_starter/controllers/responsive.dart';
-import 'package:skyconnect_starter/components/academicDetails.dart';
 import 'package:skyconnect_starter/theme/design.theme.dart';
 
 class addInstitutionAdmin extends StatefulWidget {
