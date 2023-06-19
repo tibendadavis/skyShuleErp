155e8eae64ffa00b665634404e2b09bc9cb75c95
Dave799 <davistibz@gmail.com>
2023-06-19 18:22:47 +0300

    🚀cleaned code

--- lib/screens/Data Management/designation.scrn.dart
+++ lib/screens/Data Management/designation.scrn.dart
@@ -16,12 +16,6 @@ class designation extends StatefulWidget {
 }
 
 class _designationState extends State<designation> {
-  bool _menu = false;
-  double _drawersize = 250;
-  var _classlevel;
-  var _academicYear;
-  var _stream;
-
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
