61fcc89ef4b244706968894bea9d19aa2f9a6144
Dave799 <davistibz@gmail.com>
2023-06-19 18:08:08 +0300

    🚀cleaned code

--- lib/screens/Attendance/studentAttendance.scrn.dart
+++ lib/screens/Attendance/studentAttendance.scrn.dart
@@ -21,15 +21,6 @@ class studentAttendance extends StatefulWidget {
 }
 
 class _studentAttendanceState extends State<studentAttendance> {
-  bool _menu = false;
-  double _drawersize = 250;
-  TextEditingController _From = TextEditingController();
-  TextEditingController _To = TextEditingController();
-  var _role;
-  var _classlevel;
-  var _academicYear;
-  var _stream;
-
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
