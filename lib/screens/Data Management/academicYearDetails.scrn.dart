38b17a9a775551b50a48385679357ff302a90893
Dave799 <davistibz@gmail.com>
2023-06-19 18:19:29 +0300

    🚀cleaned code

--- lib/screens/Data Management/academicYearDetails.scrn.dart
+++ lib/screens/Data Management/academicYearDetails.scrn.dart
@@ -15,12 +15,6 @@ class academicYearDetails extends StatefulWidget {
 }
 
 class _academicYearDetailState extends State<academicYearDetails> {
-  bool _menu = false;
-  double _drawersize = 250;
-  var _classlevel;
-  var _academicYear;
-  var _stream;
-
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
