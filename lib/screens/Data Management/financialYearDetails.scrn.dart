5f5bdc30cfa48548b0ee6f0d4ca4707a75860084
Dave799 <davistibz@gmail.com>
2023-06-19 18:23:18 +0300

    🚀cleaned code

--- lib/screens/Data Management/financialYearDetails.scrn.dart
+++ lib/screens/Data Management/financialYearDetails.scrn.dart
@@ -15,12 +15,6 @@ class financialYearDetails extends StatefulWidget {
 }
 
 class _financialYearDetailState extends State<financialYearDetails> {
-  bool _menu = false;
-  double _drawersize = 250;
-  var _classlevel;
-  var _academicYear;
-  var _stream;
-
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
