95642dd6e132adbd7d37736f9fdcc845220c5e29
Dave799 <davistibz@gmail.com>
2023-06-19 18:29:13 +0300

    🚀cleaned code

--- lib/screens/Exams/minorSchoolExams.scrn.dart
+++ lib/screens/Exams/minorSchoolExams.scrn.dart
@@ -17,12 +17,6 @@ class minorSchoolExams extends StatefulWidget {
 }
 
 class _supportingStaffState extends State<minorSchoolExams> {
-  bool _menu = false;
-  double _drawersize = 250;
-  var _examGroup;
-  var _academicYear;
-  var _stream;
-
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
