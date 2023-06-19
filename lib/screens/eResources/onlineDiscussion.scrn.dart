53fa45b76ca88a6eef22f071310e74a2c0a2be08
Dave799 <davistibz@gmail.com>
2023-06-19 18:27:14 +0300

    🚀cleaned code

--- lib/screens/eResources/onlineDiscussion.scrn.dart
+++ lib/screens/eResources/onlineDiscussion.scrn.dart
@@ -16,12 +16,6 @@ class onlineDiscussion extends StatefulWidget {
 }
 
 class _onlineDiscussionState extends State<onlineDiscussion> {
-  bool _menu = false;
-  double _drawersize = 250;
-  var _classlevel;
-  var _academicYear;
-  var _stream;
-
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
